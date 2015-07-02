8d1bac8d54af0ac3e06f22a19276c099b8165385
Blake Cerecero <blake@digitalblake.com>
2015-07-02 16:49:06 -0500

    [php] Cleaned up markup on the page template
    Signed-off-by: Blake Cerecero <blake@digitalblake.com>

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -72,118 +72,111 @@
  */
 ?>
 
-  <div id="page-wrapper">
-    <div id="page">
-      
-      <!-- Header -->
-      <header class="row">
-        <div class="medium-12 columns">
 
-          <?php if ($logo): ?>
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-              <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-            </a>
+<!-- Header -->
+<header class="row">
+  <div class="medium-12 columns">
+
+    <?php if ($logo): ?>
+      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+      </a>
+    <?php endif; ?>
+
+    <?php if ($site_name || $site_slogan): ?>
+      <div id="name-and-slogan">
+        <?php if ($site_name): ?>
+          <?php if ($title): ?>
+            <div id="site-name"><strong>
+              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+            </strong></div>
+          <?php else: /* Use h1 when the content title is empty */ ?>
+            <h1 id="site-name">
+              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+            </h1>
           <?php endif; ?>
-
-          <?php if ($site_name || $site_slogan): ?>
-            <div id="name-and-slogan">
-              <?php if ($site_name): ?>
-                <?php if ($title): ?>
-                  <div id="site-name"><strong>
-                    <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-                  </strong></div>
-                <?php else: /* Use h1 when the content title is empty */ ?>
-                  <h1 id="site-name">
-                    <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-                  </h1>
-                <?php endif; ?>
-              <?php endif; ?>
-
-              <?php if ($site_slogan): ?>
-                <div id="site-slogan"><?php print $site_slogan; ?></div>
-              <?php endif; ?>
-            </div> <!-- /#name-and-slogan -->
-          <?php endif; ?>
-
-          <?php print render($page['header']); ?>
-
-        </div>
-      </header> <!-- /.section, /#header -->
-      
-      <!-- Navigation --> 
-      <div class="row">
-        <div class="medium-12 columns">
-          <?php if ($main_menu || $secondary_menu): ?>
-            <nav>
-              <div class="section">
-                <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')))); ?>
-                <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')))); ?>
-              </div>
-            </nav> <!-- /.section, /#navigation -->
-          <?php endif; ?>
-
-          <?php if ($breadcrumb): ?>
-            <nav id="breadcrumb"><?php print $breadcrumb; ?></nav>
-          <?php endif; ?>
-
-          <?php print $messages; ?>
-        </div>
-      </div>
-      
-      <!-- Page Content -->
-      <div id="main" class="row">
-      
-        <!-- First Sidebar -->
-        <?php if ($page['sidebar_first']): ?>
-          <div id="sidebar-first" class="medium-4 columns">
-            <section>
-              <?php print render($page['sidebar_first']); ?>
-            </section>
-          </div>
         <?php endif; ?>
 
-        <!-- Main Content -->
-        <div id="content" class="medium-8 columns">
-          <section>
-            <?php if ($page['highlighted']): ?>
-              <div id="highlighted">
-                <?php print render($page['highlighted']); ?>
-              </div>
-            <?php endif; ?>
+        <?php if ($site_slogan): ?>
+          <div id="site-slogan"><?php print $site_slogan; ?></div>
+        <?php endif; ?>
+      </div> <!-- /#name-and-slogan -->
+    <?php endif; ?>
 
-            <a id="main-content"></a>
-            
-            <?php print render($title_prefix); ?>
+    <?php print render($page['header']); ?>
 
-            <?php if ($title): ?>
-              <h1 class="title" id="page-title"><?php print $title; ?></h1>
-            <?php endif; ?>
-            
-            <?php print render($title_suffix); ?>
+  </div>
+</header>
 
-            <?php if ($tabs): ?>
-              <div class="tabs"><?php print render($tabs); ?></div>
-            <?php endif; ?>
-          
-            <?php print render($page['help']); ?>
-          
-            <?php if ($action_links): ?>
-              <ul class="action-links">
-                <?php print render($action_links); ?>
-              </ul>
-            <?php endif; ?>
-        
-            <?php print render($page['content']); ?>
-            <?php print $feed_icons; ?>
-          </section>
+<!-- Navigation --> 
+<div class="row">
+  <div class="medium-12 columns">
+    <?php if ($main_menu || $secondary_menu): ?>
+      <nav>
+        <div class="section">
+          <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')))); ?>
+          <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')))); ?>
         </div>
+      </nav> <!-- /.section, /#navigation -->
+    <?php endif; ?>
 
-      <!-- Footer -->
-      <footer class="row">
-        <div class="medium-12 columns">
-          <?php print render($page['footer']); ?>
-        </div>
-      </footer> <!-- /.section, /#footer -->
+    <?php if ($breadcrumb): ?>
+      <nav id="breadcrumb"><?php print $breadcrumb; ?></nav>
+    <?php endif; ?>
 
+    <?php print $messages; ?>
+  </div>
+</div>
+
+<!-- Page Content -->
+<div id="main" class="row">
+
+  <!-- First Sidebar -->
+  <?php if ($page['sidebar_first']): ?>
+    <div id="sidebar-first" class="medium-4 columns">
+      <?php print render($page['sidebar_first']); ?>
     </div>
-  </div> <!-- /#page, /#page-wrapper -->
+  <?php endif; ?>
+
+  <!-- Main Content -->
+  <div id="content" class="medium-8 columns">
+    <?php if ($page['highlighted']): ?>
+      <div id="highlighted">
+        <?php print render($page['highlighted']); ?>
+      </div>
+    <?php endif; ?>
+
+    <a id="main-content"></a>
+    
+    <?php print render($title_prefix); ?>
+
+    <?php if ($title): ?>
+      <h1 class="title" id="page-title"><?php print $title; ?></h1>
+    <?php endif; ?>
+    
+    <?php print render($title_suffix); ?>
+
+    <?php if ($tabs): ?>
+      <div class="tabs"><?php print render($tabs); ?></div>
+    <?php endif; ?>
+  
+    <?php print render($page['help']); ?>
+  
+    <?php if ($action_links): ?>
+      <ul class="action-links">
+        <?php print render($action_links); ?>
+      </ul>
+    <?php endif; ?>
+
+    <?php print render($page['content']); ?>
+    <?php print $feed_icons; ?>
+  </div>
+
+</div>
+
+<!-- Footer -->
+<footer class="row">
+  <div class="medium-12 columns">
+    <?php print render($page['footer']); ?>
+  </div>
+</footer> <!-- /.section, /#footer -->
